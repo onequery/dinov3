3507b93cf609e163241979ada5be4beb3a3ef53d
heesu <assess0909@gmail.com>
2026-01-26 15:08:09 +0900

    [config] Update .gitignore and modify run_pretraining.sh for improved training setup

--- run_pretraining.sh
+++ run_pretraining.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-PYTHONPATH=${PWD} python -m dinov3.run.submit dinov3/train/train.py \
-  --nodes 1 \
+# MainGear settings (WSL)
+# PYTHONPATH=${PWD} python -m dinov3.run.submit dinov3/train/train.py \
+#   --nodes 1 \
+#   --config-file dinov3/configs/train/dinov3_vits16_pretrain.yaml \
+#   --output-dir 1_stage1_exp_pretraining \
+#   train.dataset_path=ImageNet:split=TRAIN:root=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k:extra=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k
+
+# A6000 settings (Test with Single GPU)
+export CUDA_VISIBLE_DEVICES=1
+set -e
+set -o pipefail
+PYTHONPATH=${PWD} python dinov3/train/train.py \
   --config-file dinov3/configs/train/dinov3_vits16_pretrain.yaml \
-  --output-dir 1_stage1_exp_pretraining \
-  train.dataset_path=imagenet:root=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k
+  --output-dir output/train/1_stage1_exp_pretraining \
+  train.dataset_path=ImageNet:split=TRAIN:root=/mnt/nas/external/public/raw/imagenet-1k:extra=/mnt/nas/external/public/raw/imagenet-1k
+
+# # A6000 settings (Multi-GPU)
+# PYTHONPATH=${PWD} python -m dinov3.run.submit dinov3/train/train.py \
+#   --nodes 1 \
+#   --ngpus 1 \
+#   --config-file dinov3/configs/train/dinov3_vits16_pretrain.yaml \
+#   --output-dir output/train/1_stage1_exp_pretraining \
+#   train.dataset_path=ImageNet:split=TRAIN:root=/mnt/nas/external/public/raw/imagenet-1k:extra=/mnt/nas/external/public/raw/imagenet-1k
