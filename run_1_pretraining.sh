ef3623ee2cc7aebfd5ed49690936dbb4aa053036
heesu <assess0909@gmail.com>
2026-01-30 16:31:57 +0900

    [config] Update training configuration parameters and output directory for pretraining

--- run_1_pretraining.sh
+++ run_1_pretraining.sh
@@ -6,7 +6,7 @@ set -e
 set -o pipefail
 PYTHONPATH=${PWD} python dinov3/train/train.py \
   --config-file dinov3/configs/train/dinov3_vits16_pretrain_rtx3080laptop.yaml \
-  --output-dir output/train/1_stage1_exp_pretraining \
+  --output-dir output/train/2_imagenet1k/1_stage1_pretraining \
   train.dataset_path=ImageNet:split=TRAIN:root=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k:extra=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k
 
 # A6000 settings (Test with Single GPU)
