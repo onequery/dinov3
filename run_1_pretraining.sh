3fbada6b4ae340f198600adb96f2341ad3267e0c
heesu <assess0909@gmail.com>
2026-02-04 17:02:59 +0900

    [config] Add new pretraining configuration for A6000; update training script to use the new config

--- run_1_pretraining.sh
+++ run_1_pretraining.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-# MainGear settings (WSL, Single GPU)
-export CUDA_VISIBLE_DEVICES=0
 set -e
 set -o pipefail
-PYTHONPATH=${PWD} python dinov3/train/train.py \
-  --config-file dinov3/configs/train/dinov3_vits16_pretrain_rtx3080laptop.yaml \
-  --output-dir output/train/2_imagenet1k/1_stage1_pretraining \
-  train.dataset_path=ImageNet:split=TRAIN:root=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k:extra=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k
 
-# A6000 settings (Test with Single GPU)
-# export CUDA_VISIBLE_DEVICES=1
-# set -e
-# set -o pipefail
+# ------------------------------------------------
+# MainGear settings (WSL, Single GPU)
+# export CUDA_VISIBLE_DEVICES=0
+
 # PYTHONPATH=${PWD} python dinov3/train/train.py \
-#   --config-file dinov3/configs/train/dinov3_vits16_pretrain.yaml \
-#   --output-dir output/train/1_stage1_exp_pretraining \
-#   train.dataset_path=ImageNet:split=TRAIN:root=/mnt/nas/external/public/raw/imagenet-1k:extra=/mnt/nas/external/public/raw/imagenet-1k
+#   --config-file dinov3/configs/train/dinov3_vits16_pretrain_rtx3080laptop.yaml \
+#   --output-dir output/train/2_imagenet1k/1_stage1_pretraining \
+#   train.dataset_path=ImageNet:split=TRAIN:root=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k:extra=/mnt/c/Users/heesu/workspace/dinov3_stack/input/imagenet-1k
 
+# ------------------------------------------------
+# A6000 settings (Single GPU)
+export CUDA_VISIBLE_DEVICES=0
+
+PYTHONPATH=${PWD} python dinov3/train/train.py \
+  --config-file dinov3/configs/train/dinov3_vits16_pretrain_a6000.yaml \
+  --output-dir output/a6000/1_pretrain/dinov3_vits16/2_imagenet1k/1_stage1_pretrain \
+  train.dataset_path=ImageNet:split=TRAIN:root=/mnt/nas/external/public/raw/imagenet-1k:extra=/mnt/nas/external/public/raw/imagenet-1k
+
+# ------------------------------------------------
 # # A6000 settings (Multi-GPU)
 # PYTHONPATH=${PWD} python -m dinov3.run.submit dinov3/train/train.py \
 #   --nodes 1 \
