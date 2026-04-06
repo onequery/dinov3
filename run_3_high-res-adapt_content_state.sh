097117592d70f1f2689d50fcd5e49b9907a1552c
heesu <assess0909@gmail.com>
2026-04-06 12:49:35 +0900

    [config] Update run script to set default number of dataloader workers for stability

--- run_3_high-res-adapt_content_state.sh
+++ run_3_high-res-adapt_content_state.sh
@@ -27,8 +27,13 @@ cd "${REPO_ROOT}"
 # Preserve the global batch at 16 images/step:
 #   - single GPU: batch_size_per_gpu = 16
 #   - two GPU:    batch_size_per_gpu = 8
+#
+# Reduce dataloader workers for stability on this host. This is a
+# systems/runtime knob rather than an experimental objective change, and can be
+# overridden at launch time if needed.
 
 export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1}"
+export TRAIN_NUM_WORKERS="${TRAIN_NUM_WORKERS:-8}"
 
 PYTHONPATH=${PWD} torchrun --standalone --nproc_per_node=2 -m dinov3.train.train \
     --config-file dinov3/configs/train/dinov3_vits16_high_res_adapt_content_state_a6000.yaml \
@@ -36,4 +41,5 @@ PYTHONPATH=${PWD} torchrun --standalone --nproc_per_node=2 -m dinov3.train.train
     train.dataset_path=CAGContrastFMContinuityV1:split=TRAIN:root=/mnt/nas/snubhcvc/project/cag_fm/pretrain/datasets/images \
     gram.ckpt=output/a6000/1_pretrain/dinov3_vits16/3_cagcontfm3m/2_stage2_gram_anchor/eval/training_29999/teacher_checkpoint.pth \
     student.resume_from_teacher_chkpt=output/a6000/1_pretrain/dinov3_vits16/3_cagcontfm3m/2_stage2_gram_anchor/eval/training_29999/teacher_checkpoint.pth \
-    train.batch_size_per_gpu=8
+    train.batch_size_per_gpu=8 \
+    train.num_workers="${TRAIN_NUM_WORKERS}"
